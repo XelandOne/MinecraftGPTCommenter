ce9a6e6ff88602650e9156fb19fc0d4406478e67
XelandOne <36111733+XelandOne@users.noreply.github.com>
2023-09-03 09:37:46 +0200

    Initial commit.

---
+++ src/main/java/rs/meine/Main.java
@@ -0,0 +1,14 @@
+package rs.meine;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    @Override
+    public void onEnable() {
+        System.out.println("Hello world!");
+    }
+}
